f20979c167b1d4d770c3012d0e83923931ce912c
Alejandro Padilla <alejopadilla297@gmail.com>
2023-11-27 09:30:26 -0500

    .

--- src/pages/prueba.tsx
+++ src/pages/prueba.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 
 const HTMLRenderer = () => {
   // const [htmlContent, setHtmlContent] = useState("");
