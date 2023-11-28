706af42e39e55440610ad67392df2960f25afecc
Alejandro Padilla <alejopadilla297@gmail.com>
2023-11-28 18:11:37 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ViteHtmlPlugin from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteHtmlPlugin()],
   server: {
     host: "0.0.0.0", // e.g., 'localhost' or '0.0.0.0'
   },
